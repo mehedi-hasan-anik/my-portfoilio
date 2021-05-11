9b21dbd8d315567fced6fffaf4fc64e6ef672b2e
mehedi-hasan-anik <anikm2283@gmail.com>
2021-05-11 14:04:28 +0600

    test

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './Navigation.css';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Navigation = () => {
     return (
         <div className="full-nav-area">
-         <nav class="navbar navbar-expand-lg navbar-dark bg-dark  container">
+         <nav class="navbar navbar-expand-lg   container">
             <div class="container-fluid nav-full-area">
                 <a class="navbar-brand" href="#">MD.ANIK MIAH</a>
                 <button  class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span  class="navbar-toggler-icon"></span>
+                    <span  class="navbar-toggler-icon">
+                       <FontAwesomeIcon icon={faBars} className="nav-bar-icon" />
+                    </span>
                 </button>
                 <div class="collapse navbar-collapse navigation-area" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
